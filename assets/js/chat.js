94b9ba94596edd7d192c96247ab3218e53215f87
gsylvestre <gsylvestre@gmail.com>
2017-05-18 15:27:05 +0200

    fin chat

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -2,15 +2,20 @@ console.log("dans la console du navigateur depuis chat.js")
 
 var ws = new WebSocket("ws://localhost:8080")
 
-ws.onclose = function (d) {
-    console.log(d)
-}
-ws.onerror = function (d) {
-    console.log(d)
-}
-
 ws.onopen = function(event){
     console.log("Connecté au serveur de ws")
+
+    var username = $("#username-input").val()
+
+    //envoie un message de "connexion", qui permet au serveur de connaître notre username
+    var data = {
+        "message": username,
+        "username": username,
+        "type": "login"
+    }
+
+    var json = JSON.stringify(data)
+    ws.send(json)
 
     //on écoute pour la réception de messages depuis le serveur
     ws.onmessage = function(message){
@@ -20,19 +25,35 @@ ws.onopen = function(event){
 
         var data = JSON.parse(message.data)
 
-        //crée le HTML qui sera injecté dans la fenêtre de chat
-        var messageDiv = $("<div>")
-        messageDiv.addClass("message")
-        var messageParagraph = $("<p>")
-        messageParagraph.html(data.message)
-        messageDiv.html(messageParagraph)
-
-        //ajoute notre HTML dans la liste des messages
-        $("#messages-list").append(messageDiv)
+        //en fonction du type de message qu'on vient de recevoir du serveur...
+        switch (data.type){
+            //liste des derniers messages
+            case "lastMessages":
+                for(i=0; i<data.lastMessages.length; i++){
+                    var m = data.lastMessages[i]
+                    appendMessage(m.message, m.username, m.dateSent)
+                }
+                break
+            //message normal
+            case "classic":
+                appendMessage(data.message, data.username, data.dateSent)
+                break
+            //liste des utilisateurs connectés
+            case "userlist":
+                var userUl = $("<ul>")
+                for(var i = 0; i < data.users.length; i++){
+                    userUl.append($('<li>').html(data.users[i]))
+                }
+                $("#users-list").html(userUl)
+                break
+            //on vient de recevoir un shake
+            case "shake":
+                $("#messages-list").fadeOut(100, function(){
+                    $("#messages-list").fadeIn(100)
+                })
+                break
+        }
     }
-
-
-
 
     //on met sous écoute notre formulaire, sur l'événement de soumission
     $("#message-form").on("submit", function(e){
@@ -41,8 +62,9 @@ ws.onopen = function(event){
 
         //récupère nos données dans le formulaire
         var newMessage = $("#message-input").val()
-        var username = $("#username-input").val()
+        $("#message-input").val("") //vide le champ
 
+        //envoie un message "classic"
         var data = {
             "message": newMessage,
             "username": username,
@@ -50,7 +72,45 @@ ws.onopen = function(event){
         }
 
         var json = JSON.stringify(data)
-
         ws.send(json)
     })
+
+    //mise sous écoute du bouton "shake"
+    $("#shake-btn").on("click", function(e){
+        //envoie un message shake
+        var data = {
+            "type": "shake",
+            "username": username
+        }
+        var json = JSON.stringify(data)
+        ws.send(json)
+    })
+}
+
+//ajoute un message classique dans la fenêtre de chat
+function appendMessage(content, author, date){
+    //crée le HTML qui sera injecté dans la fenêtre de chat
+    var messageDiv = $("<div>")
+    messageDiv.addClass("message")
+
+    //pour l'heure et l'auteur
+    var infoContainer = $("<em>")
+    var dateObj = new Date(date)
+    var formattedDate = dateObj.getHours() + ":" + dateObj.getMinutes() + ":" + dateObj.getSeconds()
+    moment.locale("fr")
+    formattedDate = moment(dateObj).fromNow()
+
+    infoContainer.html(author + ' (' + formattedDate + ')')
+    messageDiv.append(infoContainer)
+
+    //le message...
+    var messageParagraph = $("<p>")
+    messageParagraph.html(content)
+    messageDiv.append(messageParagraph)
+
+    //ajoute notre HTML dans la liste des messages
+    var ml = $("#messages-list")
+    ml.append(messageDiv)
+
+    ml.scrollTop(ml.prop("scrollHeight"));
 }
--- servers/WsServer.js
+++ servers/WsServer.js
@@ -4,24 +4,80 @@ const WebSocket = require('ws')
 //crée le serveur de websocket, écoute sur le port 8080
 const wss = new WebSocket.Server({'port':8080})
 
+const lastMessages = []
+
 //lorsqu'un client se connecte, la fct ci-dessous est appelée
 //on reçoit l'objet WebSocket en argument
 wss.on('connection', (ws) => {
     console.log("Nouvelle connexion !")
 
+    sendLastMessages(ws)
+
+    ws.on('close', () => {
+        sendUsersList()
+    })
+
     //déclenchée sur réception d'un message du client
     //on reçoit la chaîne envoyée en argument
     ws.on('message', (json) => {
         var data = JSON.parse(json)
-        console.log("Message reçu !")
-        console.log(data.message)
-        console.log(data.username)
+        console.log(lastMessages.length)
 
-        //on boucle sur tous les clients connectés au serveur
-        wss.clients.forEach((client) => {
-            data.dateSent = new Date()
-            client.send(JSON.stringify(data))
-        })
+        //en fonction du type de message reçu...
+        switch(data.type){
+            case "classic":
+                if (lastMessages.length >= 10){
+                    lastMessages.shift()
+                }
+                lastMessages.push(data)
+
+                //on boucle sur tous les clients connectés au serveur
+                wss.clients.forEach((client) => {
+                    data.dateSent = new Date()
+                    client.send(JSON.stringify(data))
+                })
+                break
+
+            //envoyé lorsqu'un user se connecte
+            //on reçoit ainsi son username
+            case "login":
+                //on stocke le username directement sur la websocket
+                ws.username = data.username
+
+                //on rafraichit la liste des users chez les clients
+                sendUsersList()
+                break
+
+            //shakin
+            case "shake":
+                wss.clients.forEach((client) => {
+                    if (client !== ws){
+                        client.send(json)
+                    }
+                })
+                break
+        }
     })
 })
 
+function sendLastMessages(client){
+    let data = {
+        "lastMessages": lastMessages,
+        "type": "lastMessages"
+    }
+    client.send(JSON.stringify(data))
+}
+
+function sendUsersList(){
+    let userslist = []
+    wss.clients.forEach((client) => {
+        userslist.push(client.username)
+    })
+    wss.clients.forEach((client) => {
+        data = {
+            "type": "userslist",
+            "users": userslist
+        }
+        client.send(JSON.stringify(data))
+    })
+}
